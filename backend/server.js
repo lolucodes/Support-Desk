847d967fc6c430784e049683d50b1a90a553528a
lolucodes <happycode20206@gmail.com>
2022-08-08 15:47:38 +0100

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server.....')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/api/users', (req, res) => {
+  res.status(200).json({ message: 'Welcome to my world bitches'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
