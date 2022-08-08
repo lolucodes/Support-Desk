92a6883c1848233c79e0c905c30bd4b4567f47fe
lolucodes <happycode20206@gmail.com>
2022-08-08 15:57:54 +0100

    added routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,7 @@ app.get('/api/users', (req, res) => {
   res.status(200).json({ message: 'Welcome to my world bitches'})
 })
 
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
