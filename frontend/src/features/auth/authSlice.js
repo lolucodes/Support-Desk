ecdd1edcaa59d511730fcfd59376689e3aac42c8
lolucodes <happycode20206@gmail.com>
2022-08-09 09:28:49 +0100

    user login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -35,10 +35,25 @@ export const register = createAsyncThunk(
 export const login = createAsyncThunk(
   'auth/login',
   async (user, thunkAPI) => {
-    console.log(user)
+    try {
+      return await authService.login(user)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      
+      return thunkAPI.rejectWithValue(message)
+    }
   }
 )
 
+// Logout Users
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -65,6 +80,23 @@ export const authSlice = createSlice({
         state.isError = true
         state.user = null
         state.message = action.payload
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = true
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.user = null
+        state.message = action.payload
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
       })
   }
 })
