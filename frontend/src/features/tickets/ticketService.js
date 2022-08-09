b918422c0b6b698a9f50023fb72a1b7d3a0b9c6e
lolucodes <happycode20206@gmail.com>
2022-08-09 14:56:45 +0100

    create ticket

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -3,8 +3,12 @@ import axios from 'axios'
 const API_URL = '/api/tickets/'
 
 // Create new ticket  
-const createTicket = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+const createTicket = async (ticketData, token) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}`}
+  }
+  
+  const response = await axios.post(API_URL, ticketData, config)
   
   if(response.data) {
     localStorage.setItem('ticket', JSON.stringify(response.data))
